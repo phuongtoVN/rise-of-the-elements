9193f50027eca89083c601ee2d9392812412e87e
phuongtoVN <toyenphuong10x@gmail.com>
2025-04-07 16:19:44 -0700

    final deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Performance from './components/Performance';
@@ -10,16 +10,14 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<><Header isHome={true} /><Home /></>} />
-        <Route path="/performance" element={<><Header isHome={false} /><Performance /></>} />
-        <Route path="/activities" element={<><Header isHome={false} /><Activities /></>} />
-        <Route path="/menu" element={<><Header isHome={false} /><Menu /></>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<><Header isHome={true} /><Home /></>} />
+      <Route path="/performance" element={<><Header isHome={false} /><Performance /></>} />
+      <Route path="/activities" element={<><Header isHome={false} /><Activities /></>} />
+      <Route path="/menu" element={<><Header isHome={false} /><Menu /></>} />
+      <Route path="*" element={<h1 style={{ textAlign: 'center' }}>404 - Page Not Found</h1>} />
+    </Routes>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App /> {/* No BrowserRouter here */}
-  </React.StrictMode>
+  <BrowserRouter basename="/rise-of-the-elements">
+    <App />
+  </BrowserRouter>
 );
